6c59c060323f454cc24d3b83a398dc82a7ea8a87
Gokulweb3dev <gokulweb3dev@gmail.com>
2025-04-09 22:24:48 +0530

    add line through list  feature

--- todo-app/src/component/Todolist.jsx
+++ todo-app/src/component/Todolist.jsx
@@ -8,7 +8,7 @@ const Todolist = (data) => {
   return (
     <div className='flex py-2 items-center'>
       <button onClick={()=>{data.togglelist(data.id)}}><img className='w-6' src={data.iscomplete ? tic : ntic} /></button>
-      <p className='pl-4 flex-1'>{data.text}</p>
+      <p className={`pl-4 font-medium flex-1  ${data.iscomplete? "line-through": ""}`}>{data.text}</p>
       <button onClick={()=>{data.deleteitem(data.id)}} className='w-7' ><img className='w-5' src={del} ></img></button>
     </div>
   )
